63838e9b22e89906e65422e3217a96df0875c980
Bill Vlachopoulos <blachopoulosbasileios@gmail.com>
2023-12-26 18:14:31 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 name='mlproject',
 version='0.0.1',
 author='Vasileios',
-author_email='blachopoulobasileios@gmail.com',
+author_email='blachopoulosbasileios@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
